fb451dcfd3dd589c0edfa4d019f0a7f9f8a7ee1d
Davy-Dev-Inst <d.sebbah@yahoo.fr>
2023-04-20 22:05:35 +0300

    carousel home page

---
+++ src/components/CarouselHome.js
@@ -0,0 +1,50 @@
+import Carousel from 'react-bootstrap/Carousel';
+import cascade from '../medias/cascade1920.jpg';
+import chamane from '../medias/chamane.jpeg';
+import cailloux from '../medias/cailloux1920.jpg';
+
+function CarouselHome() {
+  return (
+    <Carousel fade>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={cascade}
+          alt="cascade"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={chamane}
+          alt="chamane"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={cailloux}
+          alt="cailloux"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default CarouselHome;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import CarouselHome from "../components/CarouselHome";
 
 const Home = () => {
-    return (
-        <div>
-            HOME
-        </div>
-    );
+  return (
+    <div className="homePage">
+      <h1>
+        <i>Cindy PANTOUSTIER</i>
+        <br />
+        <span className="subtitle_homePage">
+          NATUROPATHE | MASSAGES
+          <br />
+          BIEN-ETRE
+        </span>
+      </h1>
+      <CarouselHome />
+    </div>
+  );
 };
 
 export default Home;
