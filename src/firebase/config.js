084da98a3a7e6cc029bb19d7270f492c1fa57921
DavyHaYehoudi <davyhayehoudi@gmail.com>
2023-05-23 13:25:29 +0300

    file .env.production + firebase analytics dans config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth} from "firebase/auth";
 
 const firebaseConfig = {
@@ -7,11 +8,14 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_CONFIG_PROJECT_ID,
   storageBucket: process.env.REACT_APP_CONFIG_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_CONFIG_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_CONFIG_APP_ID
+  appId: process.env.REACT_APP_CONFIG_APP_ID,
+  // pour firebase analytics
+  measurementId: process.env.REACT_APP_CONFIG_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
