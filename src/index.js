32c0b541415d595183e4557499ff095dc99a76f4
DavyHaYehoudi <davyhayehoudi@gmail.com>
2023-05-26 11:57:42 +0300

    slider consultations

--- src/index.js
+++ src/index.js
@@ -6,16 +6,13 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { AuthAdminProvider } from "./context/AuthAdminProvider";
 import { BrowserRouter } from "react-router-dom";
-import { ConsultationSelectProvider } from "./context/ConsultationSelectProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
     <AuthAdminProvider>
-        <ConsultationSelectProvider>
           <App />
-        </ConsultationSelectProvider>
     </AuthAdminProvider>
   </BrowserRouter>
 );
